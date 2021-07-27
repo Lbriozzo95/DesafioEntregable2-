9fc259c19343aa719200a086ff880f14e74c3b49
Lbriozzo95 <87342158+Lbriozzo95@users.noreply.github.com>
2021-07-27 14:46:10 -0300

    Create operaciones.ts

---
+++ operaciones.ts
@@ -0,0 +1,26 @@
+export class Operaciones {
+    private n1: number;
+    private n2: number;
+    private operation: string;
+  
+    constructor(n1: number, n2: number, operation: string) {
+      this.n1 = n1;
+      this.n2 = n2;
+      this.operation = operation;
+    }
+  
+    resultado() {
+      return new Promise((res, rej) => {
+        switch (this.operation) {
+          case "sumar":
+            res(this.n1 + this.n2);
+            break;
+          case "restar":
+            res(this.n1 - this.n2);
+          default:
+            rej("Introducir sumar o restar!");
+            break;
+        }
+      });
+    }
+  }
